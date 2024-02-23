dd1302f0191452425ea2d2003b927b1c59357e8e
Deni Sugiarto <sugiarto.deni@gmail.com>
2024-02-24 04:28:04 +0700

    update(services):make section service min height screen and reduce font size for section Decades of Experience

--- components/Home/Services.tsx
+++ components/Home/Services.tsx
@@ -6,14 +6,14 @@ const Services = () => {
   return (
     <SectionWrapper bgColor="black" id="services">
       <>
-        <div className="px-4 md:px-0 text-center mx-4 lg:mx-auto py-12 max-w-6xl">
-          <h2 className="text-2xl md:text-4xl lg:text-5xl font-open-sans font-bold leading-snug">
+        <div className="text-center mx-4 lg:mx-auto my-20 max-w-6xl">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl font-open-sans font-bold leading-snug">
             Decades of Experience Building and Operating Cloud-Native Businesses
           </h2>
         </div>
-        <div className="bg-white w-full h-full">
-          <div className="px-4 md:px-0 mx-4 lg:mx-auto py-12 max-w-7xl">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 md:gap-20">
+        <div className="bg-white w-full min-h-screen flex flex-col justify-center">
+          <div className="mx-4 lg:mx-auto my-12 max-w-7xl">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-4 md:gap-20">
               <div className="my-auto">
                 <h2 className="text-2xl md:text-4xl lg:text-5xl text-slate-900 font-bold font-open-sans mb-4 leading-none md:leading-snug">
                   More than a Financial Engineering Exercise
