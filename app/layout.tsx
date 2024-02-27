1dcc2a38dafc5b32181a8b2ca6f43ad01aa3996a
Deni Sugiarto <sugiarto.deni@gmail.com>
2024-02-27 16:02:20 +0700

    fix:remove footer and adjust text underline

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Footer from "@/components/Footer";
 import type { Metadata } from "next";
 import "../styles/globals.css";
 import { inter, openSans } from "./font";
@@ -15,13 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html className="bg-slate-950 text-slate-50 scroll-smooth" lang="en">
+    <html className="bg-black text-white scroll-smooth" lang="en">
       <body
         className={`${inter.variable} ${openSans.variable} flex flex-col items-center min-h-dvh`}
         suppressHydrationWarning
       >
         {children}
-        <Footer darkMode />
       </body>
     </html>
   );
--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -5,17 +5,19 @@ import WaterDropGrid from "../WaterDropGrid";
 const Hero = () => {
   return (
     <SectionWrapper
-      className="sm:max-w-sm px-4 md:px-0 text-center pb-12 pt-12 md:pt-18"
+      className="sm:max-w-sm px-4 md:px-0 text-center pb-12 pt-12 md:pt-24"
       id="home"
     >
       <>
         <WaterDropGrid />
-        <h1 className="underline decoration-primary decoration-4 justify-center font-bold text-4xl mt-8">
-          Songster Capital
-        </h1>
+        <div className="border-b-[5px] border-primary w-max mx-auto">
+          <h1 className="justify-center font-bold text-4xl mt-8">
+            Songster Capital
+          </h1>
+        </div>
         <p className="text-sm mt-6 md:w-[500px] md:-ml-[58px]">
-          A technology-focused private equity firm investing in the advancements
-          of cloud computing, artificial intelligence, and quantum mechanics
+          We invest in cloud computing, artificial intelligence, and the
+          commercialization of quantum mechanics
         </p>
         <Contacts color="white" />
       </>
