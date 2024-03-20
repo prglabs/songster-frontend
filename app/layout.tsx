0e4f655c9d91712863bfd9a4d7371b1a717f35b9
prglabs <148592561+prglabs@users.noreply.github.com>
2024-03-20 11:06:23 -0700

    Update footer layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import type { Metadata } from "next";
 import "../styles/globals.css";
 import { inter, openSans } from "./font";
@@ -14,12 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <html className="bg-slate-950 text-slate-50 scroll-smooth" lang="en">
     <html className="bg-black text-white scroll-smooth" lang="en">
       <body
         className={`${inter.variable} ${openSans.variable} flex flex-col items-center min-h-dvh`}
         suppressHydrationWarning
       >
         {children}
+        <Footer darkMode />
       </body>
     </html>
   );
