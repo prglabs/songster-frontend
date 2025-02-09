3d43987584743286256cbc8a6498d53b38fbc8ba
prglabs <148592561+prglabs@users.noreply.github.com>
2025-02-08 20:58:10 -0800

    Update layout.tsx

--- app/disclaimers/layout.tsx
+++ app/disclaimers/layout.tsx
@@ -1,18 +1,1 @@
-import Footer from '@/components/Footer';
-import { cn } from '@/lib/utils';
-import { PropsWithChildren } from 'react';
-import { inter, openSans } from '../font';
 
-export default function RootLayout({ children }: PropsWithChildren) {
-  return (
-    <html className='scroll-smooth' lang='en'>
-      <body
-        className={cn(inter.variable, openSans.variable, 'font-body')}
-        suppressHydrationWarning
-      >
-        {children}
-        <Footer darkMode />
-      </body>
-    </html>
-  );
-}
