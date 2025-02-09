f4679508db1d9e14f90285047a64b852bb1fd477
prglabs <148592561+prglabs@users.noreply.github.com>
2025-02-08 20:52:15 -0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,5 +2,11 @@ import { footerProps } from '@/@types/general';
 import { cn } from '@/lib/utils';
 import React from 'react';
 
+function Footer({ darkMode = false, asDiv = false }: footerProps) {
+
+  return (
+  );
+}
+
 
 export default Footer;
